bf4a9715e44462598da6bf8bd6e7aa39fef6013b
sulochan barakoti <sulochanbarakoti09@gmail.com>
2022-03-15 22:53:35 +0200

    complete

---
+++ src/components/total.js
@@ -0,0 +1,11 @@
+const Total = (props) => {
+    return ( 
+        <div>
+            <p>
+                Number of exercises {props.total}
+            </p>
+        </div>
+     );
+}
+
+export default Total;
